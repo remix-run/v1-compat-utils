e9ca66c0130f48b705cc2cd7b4fa4a941827f448
Logan McAnsh <logan@mcan.sh>
2023-03-27 14:23:57 -0400

    test: update routesDirectory
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/route-convention/__tests__/index-test.ts
+++ packages/route-convention/__tests__/index-test.ts
@@ -110,7 +110,7 @@ describe("defineConventionalRoutes", () => {
   it("creates a route manifest from the routes directory", () => {
     let routes = createRoutesFromFolders(
       defineRoutes,
-      path.join(__dirname, "..", "..", "..", "example", "app")
+      path.join(__dirname, "..", "..", "..", "example", "app", "routes")
     );
     expect(routes).toMatchInlineSnapshot(`
       {
